79d3a25ab101fce7fc10fb3635fc2f6d3794a0d6
cymun111 <cymun111@gmail.com>
2017-08-04 21:07:54 -0700

    fixing if statements

--- Bing-bong.js
+++ Bing-bong.js
@@ -3,16 +3,17 @@ $(document).ready(function(){
 $("#myButton").click(function(){
    var myArr = [];
               for (i = 1; i < 101; ++i){
-                 if (i%3 === 0){
+
+                if (i%3 === 0 && i%5 === 0){
+                  myArr.push("bing bong");}
+
+                 else if (i%3 === 0){
                    myArr.push("bing");}
 
-                 if (i%5 === 0){
+                 else if (i%5 === 0){
                    myArr.push("bong");}
 
-                 if (i%3 === 0 && i%5 === 0){
-                   myArr.push("bing bong");}
-
-                 if( ( i % 3 !== 0 ) && ( i % 5 !== 0 ) ){
+                 else if( ( i % 3 !== 0 ) && ( i % 5 !== 0 ) ){
                        myArr.push(i);}
                  }
 document.getElementById("myId1").innerHTML = myArr;
